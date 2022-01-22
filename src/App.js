e2fa9e512938029e75a1a81c22b6e373db8272e1
vinhqngo5 <vinhqngo5@gmail.com>
2022-01-22 10:25:38 +0700

    BugFix: fix overflow in bottom of website

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ const useStyles = makeStyles((theme) => {
 				width: 0 /* Remove scrollbar space */,
 				background: "transparent" /* Optional: just make scrollbar invisible */,
 			},
+			overflow: "hidden"
 		},
 	};
 });
